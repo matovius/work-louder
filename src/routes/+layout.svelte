02b89030fdab95c20a7bb7a0f6a7f92f2942a320
Martin Matovu <matovius@proton.me>
2023-11-13 09:54:22 +0300

    Implement header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,11 @@
 	import '@fontsource-variable/handjet';
 
 	import '../app.css';
+
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<div class="w-full bg-black min-h-[100dvh]">
+<div class="text-white w-full bg-black min-h-[100dvh]">
+	<Header />
 	<slot />
 </div>
