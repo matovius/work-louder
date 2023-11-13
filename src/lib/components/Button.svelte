b5e0460c8601be84109d920297222cd253d7aca9
Martin Matovu <matovius@proton.me>
2023-11-13 16:48:21 +0300

    Add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let variant:
+		| 'solid-white'
+		| 'solid-black'
+		| 'solid-primary'
+		| 'translucent-white'
+		| 'translucent-black'
+		| 'translucent-primary'
+		| 'ghost-white'
+		| 'ghost-black'
+		| 'ghost-primary';
+
+	export let as: 'button' | 'link';
+	export let url: string = '/';
+</script>
+
+{#if as === 'button'}
+	<button class={`button ${variant}`}>
+		<slot />
+	</button>
+{:else if as === 'link'}
+	<a href={url} class={`button ${variant}`}>
+		<slot />
+	</a>
+{/if}
