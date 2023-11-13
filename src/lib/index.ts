84ede65789c3f4225a20242bc916971cb33c53d0
Martin Matovu <matovius@proton.me>
2023-11-13 16:49:12 +0300

    Export all new component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 export * from './components/Header.svelte';
 export * from './components/icons/WLLogoFull.svelte';
 export * from './components/icons/WLLogoShort.svelte';
+export * from './components/InfiniteCarousel.svelte';
+export * from './components/Footer.svelte';
+export * from './components/Button.svelte';
