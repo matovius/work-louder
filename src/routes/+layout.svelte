182d06fd282851e4606eaab12ccae8acaa9ca6eb
Martin Matovu <matovius@proton.me>
2023-11-14 18:39:00 +0300

    Move main continer wrapper into layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,10 @@
 
 <div class="text-white w-full bg-black min-h-[100dvh]">
 	<Header />
-	<slot />
+	<main class="w-full flex justify-center items-center p-24">
+		<div class="container flex flex-col justify-center items-center gap-48">
+			<slot />
+		</div>
+	</main>
 	<Footer />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,227 +16,221 @@
 	<title>work_louder</title>
 </svelte:head>
 
-<main class="w-full flex justify-center items-center p-24">
-	<div class="container flex flex-col justify-center items-center gap-48">
-		<section id="hero" class="w-full flex justify-center items-center">
-			<div
-				class="w-full h-480 flex flex-col justify-center items-center gap-48 bg-white/20 rounded overflow-hidden relative"
-				style={`
-					background-image: linear-gradient(hsl(0, 0%, 0%, 0.2), hsl(0, 0%, 0%, 0.2)), url(${nomadForHeroSection});
-					background-position: center;
-					background-size: cover;
-					background-repeat: no-repeat;
-					`}
-			>
-				<h1 class="h1 text-center relative">
-					<span>focus.</span>
-					<br />
-					<span>work.</span>
-					<br />
-					<span>rest.</span>
+<section id="hero" class="w-full flex justify-center items-center">
+	<div
+		class="w-full h-480 flex flex-col justify-center items-center gap-48 bg-white/20 rounded overflow-hidden relative"
+		style={`
+			background-image: linear-gradient(hsl(0, 0%, 0%, 0.2), hsl(0, 0%, 0%, 0.2)), url(${nomadForHeroSection});
+			background-position: center;
+			background-size: cover;
+			background-repeat: no-repeat;
+			`}
+	>
+		<h1 class="h1 text-center relative">
+			<span>focus.</span>
+			<br />
+			<span>work.</span>
+			<br />
+			<span>rest.</span>
 
-					<div class="p-4 bg-lemon-lime text-black rounded-full absolute top-0 right-0">
-						<Asterisk />
-					</div>
-				</h1>
-				<Button as="link" variant="solid-primary">
-					<span class="paragraph">discover the nomad</span>
-				</Button>
-				<div
-					id="asterisk"
-					class="absolute bottom-0 tablet-lg:left-0 w-full tablet-lg:w-240 tablet-lg:max-w-[15rem] flex flex-row justify-center items-center tablet-lg:flex-col tablet-lg:justify-start tablet-lg:items-start gap-12 p-24"
-				>
-					<div class="w-fit h-fit p-4 bg-lemon-lime text-black rounded-full">
-						<Asterisk />
-					</div>
-					<div class="w-full">
-						<span class="paragraph">our tools help you focus in ways you've only dreamt of.</span>
-					</div>
-				</div>
+			<div class="p-4 bg-lemon-lime text-black rounded-full absolute top-0 right-0">
+				<Asterisk />
 			</div>
-		</section>
-
-		<InfiniteCarousel animationDuration="slow">
-			<span>know productivity, efficiency, passion, and creativity with no limits.</span>
-		</InfiniteCarousel>
-
-		<section
-			id="featured-products"
-			class="w-full flex justify-start items-center flex-wrap gap-24 py-24"
+		</h1>
+		<Button as="link" variant="solid-primary">
+			<span class="paragraph">discover the nomad</span>
+		</Button>
+		<div
+			id="asterisk"
+			class="absolute bottom-0 tablet-lg:left-0 w-full tablet-lg:w-240 tablet-lg:max-w-[15rem] flex flex-row justify-center items-center tablet-lg:flex-col tablet-lg:justify-start tablet-lg:items-start gap-12 p-24"
 		>
-			<div
-				id="main-product-card"
-				class="w-full flex flex-col tablet-sm:flex-row justify-center items-center gap-24"
-			>
-				<div
-					class="w-full max-w-[53.5rem] h-360 bg-white/10 rounded overflow-hidden"
-					style={`
-						background-image: url(${nomadColorways});
-						background-position: center;
-						background-size: cover;
-						background-repeat: no-repeat;
-						`}
-				/>
-				<div
-					class="text-black w-full tablet-sm:max-w-[22.5rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-lemon-lime"
-				>
-					<div class="w-full flex flex-col gap-12">
-						<h3 class="h3">nomad [e]</h3>
-						<span class="h5 text-black/60">keyboard</span>
-						<span class="small">[e] for everyone</span>
-					</div>
-					<div class="w-full">
-						<p class="paragraph">
-							the first keyboard to get you in the zone, and keep you there. currently on pre-order,
-							you can expect to get the nomad [e] by Q1, 2024.
-						</p>
-					</div>
-					<div class="w-full">
-						<Button as="button" variant="solid-white">
-							<span>explore</span>
-						</Button>
-					</div>
-				</div>
+			<div class="w-fit h-fit p-4 bg-lemon-lime text-black rounded-full">
+				<Asterisk />
 			</div>
-			<div
-				id="small-product-card"
-				class="w-full tablet-sm:max-w-[calc(50%-12px)] tablet-lg:max-w-none laptop:w-[calc(50%-12px)] flex flex-col tablet-lg:flex-row-reverse justify-center items-center gap-24"
-			>
-				<div
-					class="w-full laptop:max-w-[17.5rem] h-360 bg-white/10 rounded overflow-hidden"
-					style={`
-						background-image: url(${creatorMicro});
-						background-position: center;
-						background-size: cover;
-						background-repeat: no-repeat;
-						`}
-				/>
-				<div
-					class="text-black w-full tablet-lg:max-w-[19rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
-				>
-					<div class="w-full flex flex-col gap-12">
-						<h3 class="h3">creator micro</h3>
-						<span class="h5 text-black/60">keypad</span>
-					</div>
-					<div class="w-full">
-						<p class="paragraph">
-							everything you love and want from the creator board, and a little more, distilled into
-							a compact device.
-						</p>
-					</div>
-					<div class="w-full">
-						<Button as="button" variant="solid-primary">
-							<span>explore</span>
-						</Button>
-					</div>
-				</div>
+			<div class="w-full">
+				<span class="paragraph">our tools help you focus in ways you've only dreamt of.</span>
 			</div>
-			<div
-				id="small-product-card"
-				class="w-full tablet-sm:max-w-[calc(50%-12px)] tablet-lg:max-w-none laptop:w-[calc(50%-12px)] flex flex-col tablet-lg:flex-row justify-center items-center gap-24"
-			>
-				<div
-					class="w-full laptop:max-w-[17.5rem] h-360 bg-white/10 rounded overflow-hidden"
-					style={`
-						background-image: url(${creatorBoard});
-						background-position: center;
-						background-size: cover;
-						background-repeat: no-repeat;
-						`}
-				/>
-				<div
-					class="text-black w-full tablet-lg:max-w-[19rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
-				>
-					<div class="w-full flex flex-col gap-12">
-						<h3 class="h3">creator board</h3>
-						<span class="h5 text-black/60">keyboard</span>
-					</div>
-					<div class="w-full">
-						<p class="paragraph">
-							the first modular keyboard for digital creators, designed for those who defy limits
-							and change the world.
-						</p>
-					</div>
-					<div class="w-full">
-						<Button as="button" variant="solid-primary">
-							<span>explore</span>
-						</Button>
-					</div>
-				</div>
-			</div>
-			<div
-				id="wide-product-card"
-				class="w-full flex flex-col-reverse tablet-sm:flex-row justify-center items-center gap-24"
-			>
-				<div
-					class="text-black w-full tablet-lg:max-w-[22.5rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
-				>
-					<div class="w-full flex flex-col gap-12">
-						<h3 class="h3">creator board XL</h3>
-						<span class="h5 text-black/60">keyboard</span>
-					</div>
-					<div class="w-full">
-						<p class="paragraph">
-							sometimes bigger is better. expand your creativity coming Q1, 2024.
-						</p>
-					</div>
-					<div class="w-full">
-						<Button as="button" variant="solid-primary">
-							<span>explore</span>
-						</Button>
-					</div>
-				</div>
-				<div
-					class="w-full h-360 bg-white/10 rounded overflow-hidden"
-					style={`
-						background-image: url(${creatorBoardXL});
-						background-position: center;
-						background-size: cover;
-						background-repeat: no-repeat;
-						`}
-				/>
-			</div>
-		</section>
-
-		<InfiniteCarousel animationDuration="slow">
-			<span>all work & <s>no</s> play - work_louder, inc.</span>
-		</InfiniteCarousel>
-
-		<section id="extra-ctas" class="w-full flex justify-start items-center flex-wrap gap-24 py-24">
-			<div
-				id="storefront-cta"
-				class="w-608 h-360 flex flex-col justify-center items-center gap-24 p-24 rounded overflow-hidden bg-white/10"
-				style={`
-					background-image: linear-gradient(hsl(0, 0%, 0%, 0.4), hsl(0, 0%, 0%, 0.4)), url(${floatingKeycaps});
-					background-position: center;
-					background-size: cover;
-					background-repeat: no-repeat;
-					`}
-			>
-				<h2 class="h2">our products</h2>
-				<p class="paragraph text-center">
-					for all the keyboards, deskmats, keycaps, modules, collabs, and other merchandise.
-				</p>
-				<Button as="link" variant="solid-primary">
-					<span>head to the store</span>
-				</Button>
-			</div>
-			<div
-				id="wrkshop-cta"
-				class="w-608 h-360 flex flex-col justify-center items-center gap-24 p-24 rounded overflow-hidden bg-white/10"
-				style={`
-					background-image: linear-gradient(hsl(0, 0%, 0%, 0.4), hsl(0, 0%, 0%, 0.4)), url(${clothingModel});
-					background-position: center;
-					background-size: cover;
-					background-repeat: no-repeat;
-					`}
-			>
-				<h2 class="h2">wrk.shop</h2>
-				<p class="paragraph text-center">where your renders become reality</p>
-				<Button as="button" variant="translucent-black">
-					<span>coming soon</span>
-				</Button>
-			</div>
-		</section>
+		</div>
 	</div>
-</main>
+</section>
+
+<InfiniteCarousel animationDuration="slow">
+	<span>know productivity, efficiency, passion, and creativity with no limits.</span>
+</InfiniteCarousel>
+
+<section
+	id="featured-products"
+	class="w-full flex justify-start items-center flex-wrap gap-24 py-24"
+>
+	<div
+		id="main-product-card"
+		class="w-full flex flex-col tablet-sm:flex-row justify-center items-center gap-24"
+	>
+		<div
+			class="w-full max-w-[53.5rem] h-360 bg-white/10 rounded overflow-hidden"
+			style={`
+				background-image: url(${nomadColorways});
+				background-position: center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				`}
+		/>
+		<div
+			class="text-black w-full tablet-sm:max-w-[22.5rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-lemon-lime"
+		>
+			<div class="w-full flex flex-col gap-12">
+				<h3 class="h3">nomad [e]</h3>
+				<span class="h5 text-black/60">keyboard</span>
+				<span class="small">[e] for everyone</span>
+			</div>
+			<div class="w-full">
+				<p class="paragraph">
+					the first keyboard to get you in the zone, and keep you there. currently on pre-order, you
+					can expect to get the nomad [e] by Q1, 2024.
+				</p>
+			</div>
+			<div class="w-full">
+				<Button as="button" variant="solid-white">
+					<span>explore</span>
+				</Button>
+			</div>
+		</div>
+	</div>
+	<div
+		id="small-product-card"
+		class="w-full tablet-sm:max-w-[calc(50%-12px)] tablet-lg:max-w-none laptop:w-[calc(50%-12px)] flex flex-col tablet-lg:flex-row-reverse justify-center items-center gap-24"
+	>
+		<div
+			class="w-full laptop:max-w-[17.5rem] h-360 bg-white/10 rounded overflow-hidden"
+			style={`
+				background-image: url(${creatorMicro});
+				background-position: center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				`}
+		/>
+		<div
+			class="text-black w-full tablet-lg:max-w-[19rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
+		>
+			<div class="w-full flex flex-col gap-12">
+				<h3 class="h3">creator micro</h3>
+				<span class="h5 text-black/60">keypad</span>
+			</div>
+			<div class="w-full">
+				<p class="paragraph">
+					everything you love and want from the creator board, and a little more, distilled into a
+					compact device.
+				</p>
+			</div>
+			<div class="w-full">
+				<Button as="button" variant="solid-primary">
+					<span>explore</span>
+				</Button>
+			</div>
+		</div>
+	</div>
+	<div
+		id="small-product-card"
+		class="w-full tablet-sm:max-w-[calc(50%-12px)] tablet-lg:max-w-none laptop:w-[calc(50%-12px)] flex flex-col tablet-lg:flex-row justify-center items-center gap-24"
+	>
+		<div
+			class="w-full laptop:max-w-[17.5rem] h-360 bg-white/10 rounded overflow-hidden"
+			style={`
+				background-image: url(${creatorBoard});
+				background-position: center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				`}
+		/>
+		<div
+			class="text-black w-full tablet-lg:max-w-[19rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
+		>
+			<div class="w-full flex flex-col gap-12">
+				<h3 class="h3">creator board</h3>
+				<span class="h5 text-black/60">keyboard</span>
+			</div>
+			<div class="w-full">
+				<p class="paragraph">
+					the first modular keyboard for digital creators, designed for those who defy limits and
+					change the world.
+				</p>
+			</div>
+			<div class="w-full">
+				<Button as="button" variant="solid-primary">
+					<span>explore</span>
+				</Button>
+			</div>
+		</div>
+	</div>
+	<div
+		id="wide-product-card"
+		class="w-full flex flex-col-reverse tablet-sm:flex-row justify-center items-center gap-24"
+	>
+		<div
+			class="text-black w-full tablet-lg:max-w-[22.5rem] h-360 flex flex-col gap-24 p-24 rounded overflow-hidden bg-white"
+		>
+			<div class="w-full flex flex-col gap-12">
+				<h3 class="h3">creator board XL</h3>
+				<span class="h5 text-black/60">keyboard</span>
+			</div>
+			<div class="w-full">
+				<p class="paragraph">sometimes bigger is better. expand your creativity coming Q1, 2024.</p>
+			</div>
+			<div class="w-full">
+				<Button as="button" variant="solid-primary">
+					<span>explore</span>
+				</Button>
+			</div>
+		</div>
+		<div
+			class="w-full h-360 bg-white/10 rounded overflow-hidden"
+			style={`
+				background-image: url(${creatorBoardXL});
+				background-position: center;
+				background-size: cover;
+				background-repeat: no-repeat;
+				`}
+		/>
+	</div>
+</section>
+
+<InfiniteCarousel animationDuration="slow">
+	<span>all work & <s>no</s> play - work_louder, inc.</span>
+</InfiniteCarousel>
+
+<section id="extra-ctas" class="w-full flex justify-start items-center flex-wrap gap-24 py-24">
+	<div
+		id="storefront-cta"
+		class="w-608 h-360 flex flex-col justify-center items-center gap-24 p-24 rounded overflow-hidden bg-white/10"
+		style={`
+			background-image: linear-gradient(hsl(0, 0%, 0%, 0.4), hsl(0, 0%, 0%, 0.4)), url(${floatingKeycaps});
+			background-position: center;
+			background-size: cover;
+			background-repeat: no-repeat;
+			`}
+	>
+		<h2 class="h2">our products</h2>
+		<p class="paragraph text-center">
+			for all the keyboards, deskmats, keycaps, modules, collabs, and other merchandise.
+		</p>
+		<Button as="link" variant="solid-primary">
+			<span>head to the store</span>
+		</Button>
+	</div>
+	<div
+		id="wrkshop-cta"
+		class="w-608 h-360 flex flex-col justify-center items-center gap-24 p-24 rounded overflow-hidden bg-white/10"
+		style={`
+			background-image: linear-gradient(hsl(0, 0%, 0%, 0.4), hsl(0, 0%, 0%, 0.4)), url(${clothingModel});
+			background-position: center;
+			background-size: cover;
+			background-repeat: no-repeat;
+			`}
+	>
+		<h2 class="h2">wrk.shop</h2>
+		<p class="paragraph text-center">where your renders become reality</p>
+		<Button as="button" variant="translucent-black">
+			<span>coming soon</span>
+		</Button>
+	</div>
+</section>
