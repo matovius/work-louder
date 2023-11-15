4c947bf1efcc062fb23e4b256d4691dbb47b6cd2
Martin Matovu <matovius@proton.me>
2023-11-16 00:15:02 +0300

    Rename deskmats store component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,7 +5,7 @@ export * from './components/InfiniteCarousel.svelte';
 export * from './components/Footer.svelte';
 export * from './components/Button.svelte';
 
-export * from './components/store/Deskmats.svelte';
+export * from './components/store/DeskmatsSection.svelte';
 export * from './components/store/KeyboardsSection.svelte';
 export * from './components/store/KeycapsSection.svelte';
 export * from './components/store/MerchandiseSection.svelte';
