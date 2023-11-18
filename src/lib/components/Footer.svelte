ff5e6bfbd81fe8c965e55e37d581b0697043cf9b
Martin Matovu <matovius@proton.me>
2023-11-18 14:52:15 +0300

    Implement new link component and data

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,8 @@
 	import InfiniteCarousel from './InfiniteCarousel.svelte';
 	import WlLogoFull from './icons/WLLogoFull.svelte';
 	import founderAvatar from '../images/avatar.png';
+	import { NavLinks, SocialLinks, DummyLegalLinks } from './Links';
+	import Link from './Link.svelte';
 </script>
 
 <footer class="w-full flex justify-center items-center">
@@ -10,9 +12,11 @@
 			<InfiniteCarousel background="solid-primary">
 				<span>human after all.</span>
 			</InfiniteCarousel>
+
 			<div class="h-36 mob-lg:h-48 text-white">
 				<WlLogoFull />
 			</div>
+
 			<div class="w-fit h-96">
 				<img
 					src={founderAvatar}
@@ -21,57 +25,25 @@
 				/>
 			</div>
 		</div>
+
 		<div id="bottom" class="w-full flex flex-col justify-start items-center gap-48">
-			<div class="w-full flex flex-col gap-24">
-				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-24">
-					<li>
-						<a href="/store" class="paragraph text-white/60">store</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">setup</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">about</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">updates</a>
-					</li>
+			<div class="w-full flex flex-col gap-12">
+				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-y-12">
+					{#each NavLinks as link}
+						<Link type="internal" label={link.label} url={link.url} />
+					{/each}
 				</ul>
 
-				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-24">
-					<li>
-						<a href="/" class="paragraph text-white/60">twitter/x</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">instagram</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">tiktok</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">threads</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">discord</a>
-					</li>
+				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-y-12">
+					{#each SocialLinks as link}
+						<Link type="external" label={link.label} url={link.url} />
+					{/each}
 				</ul>
 
-				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-24">
-					<li>
-						<a href="/" class="paragraph text-white/60">affiliate program</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">terms of service</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">return policy</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">privacy policy</a>
-					</li>
-					<li>
-						<a href="/" class="paragraph text-white/60">terms and conditions</a>
-					</li>
+				<ul class="w-full flex flex-row justify-center items-center flex-wrap gap-y-12">
+					{#each DummyLegalLinks as link}
+						<Link type="internal" label={link.label} url={link.url} />
+					{/each}
 				</ul>
 			</div>
 
