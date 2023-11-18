9b7be29c82e99a2e2ec466bb67ead2bc9949f90e
Martin Matovu <matovius@proton.me>
2023-11-18 12:27:15 +0300

    Export flair component and product components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,9 +4,15 @@ export * from './components/icons/WLLogoShort.svelte';
 export * from './components/InfiniteCarousel.svelte';
 export * from './components/Footer.svelte';
 export * from './components/Button.svelte';
+export * from './components/Flair.svelte';
 
 export * from './components/store/DeskmatsSection.svelte';
 export * from './components/store/KeyboardsSection.svelte';
 export * from './components/store/KeycapsSection.svelte';
 export * from './components/store/MerchandiseSection.svelte';
 export * from './components/store/ModulesSection.svelte';
+
+export * from './components/product-card/ProductImage.svelte';
+export * from './components/product-card/ProductDetails.svelte';
+export * from './components/product-card/ProductCard.svelte';
+export * from './components/product-card/ProductData';
