d6fd7609494943527d6180fffa963b6cf28eac0f
Martin Matovu <matovius@proton.me>
2023-11-18 12:25:09 +0300

    Add component

---
+++ src/lib/components/Flair.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let variant: 'green-chip' | 'orange-chip' | 'gray-chip' | 'blue-chip' | 'pink-chip';
+	export let label: string;
+</script>
+
+<div class={`flair-chip ${variant}`}>
+	{label}
+</div>
