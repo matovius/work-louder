619b8473a8fab5926ef55136b7d6c31daf669580
Martin Matovu <matovius@proton.me>
2023-11-18 14:48:29 +0300

    Add component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let type: 'internal' | 'external';
+	export let label: string;
+	export let url: string;
+</script>
+
+{#if type === 'internal'}
+	<a href={url} class="link">
+		{label}
+	</a>
+{:else if type === 'external'}
+	<a href={url} target="_blank" rel="noopener noreferrer" class="link">
+		{label}
+	</a>
+{/if}
