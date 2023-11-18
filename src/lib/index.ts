33097e91f0eceef60f3d4159349c498cd2d2b561
Martin Matovu <matovius@proton.me>
2023-11-18 14:48:17 +0300

    Export link data and component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -16,3 +16,6 @@ export * from './components/product-card/ProductImage.svelte';
 export * from './components/product-card/ProductDetails.svelte';
 export * from './components/product-card/ProductCard.svelte';
 export * from './components/product-card/ProductData';
+
+export * from './components/Links';
+export * from './components/Link.svelte';
