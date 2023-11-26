3fdddfc68c1c80f5e26d84fdc9a660df6e5c0700
Martin Matovu <matovius@proton.me>
2023-11-26 20:29:49 +0300

    Remove unnecessary exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export * from './components/Header.svelte';
 export * from './components/icons/WLLogoFull.svelte';
 export * from './components/icons/WLLogoShort.svelte';
 export * from './components/InfiniteCarousel.svelte';
+export * from './components/Marquee.svelte';
 export * from './components/Footer.svelte';
 export * from './components/Button.svelte';
 export * from './components/Flair.svelte';
@@ -15,7 +16,5 @@ export * from './components/store/ModulesSection.svelte';
 export * from './components/product-card/ProductImage.svelte';
 export * from './components/product-card/ProductDetails.svelte';
 export * from './components/product-card/ProductCard.svelte';
-export * from './components/product-card/ProductData';
 
-export * from './components/Links';
 export * from './components/Link.svelte';
