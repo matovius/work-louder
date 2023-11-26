cb110ad36171e1f8c6003c94c7c07d43353e0d12
Martin Matovu <matovius@proton.me>
2023-11-26 20:21:27 +0300

    Improve element layouts

--- src/lib/components/product-card/ProductCard.svelte
+++ src/lib/components/product-card/ProductCard.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import Flair from '../Flair.svelte';
-	import ProductImage from './ProductImage.svelte';
-	import ProductDetails from './ProductDetails.svelte';
+	import Flair from '$lib/components/Flair.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import { CartItemsStore, CartPrice } from '$lib/stores/Store';
+	import { onDestroy } from 'svelte';
 
-	export let size: 'large' | 'medium' | 'small';
+	export let productID: string;
+	export let size: 'large' | 'small';
 	export let name: string;
 	export let category: 'keyboard' | 'keypad' | 'module' | 'keycaps' | 'merchandise' | 'deskmat';
-	export let price: string;
+	export let price: number;
 	export let imgSrc: string;
 	export let imgAlt: string;
 	export let url: string = '#';
@@ -15,141 +17,179 @@
 	export let grayFlairs: string[] | undefined = undefined;
 	export let blueFlairs: string[] | undefined = undefined;
 	export let pinkFlairs: string[] | undefined = undefined;
+
+	let cartItems: string[];
+
+	const unsubscribeItems = CartItemsStore.subscribe((data) => {
+		cartItems = data;
+	});
+
+	function addItemToCart() {
+		if (cartItems[0] === 'empty') {
+			CartItemsStore.set([productID]);
+			CartPrice.set(price);
+			return;
+		} else if (cartItems.includes(productID)) {
+			console.log('item is in cart already');
+			return;
+		}
+
+		CartItemsStore.update((currentData) => {
+			return [productID, ...currentData];
+		});
+
+		CartPrice.update((currentPrice) => {
+			return currentPrice + price;
+		});
+
+		//console.log(cartItems);
+		//console.log($CartItemsStore);
+	}
+
+	onDestroy(unsubscribeItems);
 </script>
 
 {#if size === 'large'}
-	<a
-		href={url}
+	<div
 		id="product-card__large"
-		class="isolate w-full min-w-full flex flex-col justify-center items-center gap-24 p-12 pb-24 hover:bg-white/5 rounded-t-[32px] rounded-b relative outline outline-2 outline-transparent outline-offset-4 focus-visible:outline-current"
+		class=" isolate w-full min-w-full h-fit laptop:h-360 flex flex-col laptop:flex-row justify-center items-center gap-24 bg-black relative outline outline-2 outline-transparent outline-offset-4 focus-visible:outline-current large-card"
 	>
 		<div
-			id="flairs"
-			class="absolute top-0 right-0 z-10 w-full p-36 flex justify-start items-start gap-4"
+			class="isolate w-full h-240 tablet-sm:h-360 laptop:h-full flex justify-center items-center rounded border border-white/20 overflow-hidden relative"
 		>
-			{#if greenFlairs !== undefined}
-				{#each greenFlairs as flair}
-					<Flair variant="green-chip" label={flair} />
-				{/each}
-			{/if}
+			<img src={imgSrc} alt={imgAlt} class="w-full h-full object-cover" />
 
-			{#if orangeFlairs !== undefined}
-				{#each orangeFlairs as flair}
-					<Flair variant="orange-chip" label={flair} />
-				{/each}
-			{/if}
+			<div id="flairs" class="w-full justify-start items-start gap-4 absolute top-0 left-0 p-24">
+				{#if greenFlairs !== undefined}
+					{#each greenFlairs as flair}
+						<Flair variant="green-chip" label={flair} />
+					{/each}
+				{/if}
 
-			{#if grayFlairs !== undefined}
-				{#each grayFlairs as flair}
-					<Flair variant="gray-chip" label={flair} />
-				{/each}
-			{/if}
-			{#if blueFlairs !== undefined}
-				{#each blueFlairs as flair}
-					<Flair variant="blue-chip" label={flair} />
-				{/each}
-			{/if}
+				{#if orangeFlairs !== undefined}
+					{#each orangeFlairs as flair}
+						<Flair variant="orange-chip" label={flair} />
+					{/each}
+				{/if}
 
-			{#if pinkFlairs !== undefined}
-				{#each pinkFlairs as flair}
-					<Flair variant="pink-chip" label={flair} />
-				{/each}
-			{/if}
-			<div />
+				{#if grayFlairs !== undefined}
+					{#each grayFlairs as flair}
+						<Flair variant="gray-chip" label={flair} />
+					{/each}
+				{/if}
+				{#if blueFlairs !== undefined}
+					{#each blueFlairs as flair}
+						<Flair variant="blue-chip" label={flair} />
+					{/each}
+				{/if}
+
+				{#if pinkFlairs !== undefined}
+					{#each pinkFlairs as flair}
+						<Flair variant="pink-chip" label={flair} />
+					{/each}
+				{/if}
+				<div />
+			</div>
+
+			<div
+				class="w-fit flex flex-row flex-wrap justify-start items-start gap-4 p-24 absolute bottom-0 left-0"
+			>
+				<Button as="button" variant="solid-black" on:click={addItemToCart}>
+					<span>add to cart</span>
+				</Button>
+
+				<Button as="link" {url} variant="solid-black">
+					<span>see details</span>
+				</Button>
+			</div>
 		</div>
 
-		<ProductImage {imgSrc} {imgAlt} />
-
-		<ProductDetails {name} {category} {price} />
-	</a>
-{:else if size === 'medium'}
-	<a
-		href={url}
-		id="medium"
-		class="isolate w-full tablet-lg:max-w-[calc(50%-12px)] flex flex-col justify-center items-center gap-24 p-12 pb-24 hover:bg-white/5 rounded-t-[32px] rounded-b relative outline outline-2 outline-transparent outline-offset-4 focus-visible:outline-current"
-	>
 		<div
-			id="flairs"
-			class="absolute top-0 right-0 z-10 w-full p-36 flex justify-start items-start gap-4"
+			id="product-details-wrapper"
+			class="isolate w-full laptop:max-w-[15rem] h-full flex flex-col justify-start items-start gap-24 p-24 bg-black rounded border border-white/20 overflow-hidden"
 		>
-			{#if greenFlairs !== undefined}
-				{#each greenFlairs as flair}
-					<Flair variant="green-chip" label={flair} />
-				{/each}
-			{/if}
+			<div class="w-full max-w-[22.5rem] h-full flex flex-col justify-start items-start gap-24">
+				<div class="flex flex-col gap-12">
+					<h4 class="h4">{name}</h4>
 
-			{#if orangeFlairs !== undefined}
-				{#each orangeFlairs as flair}
-					<Flair variant="orange-chip" label={flair} />
-				{/each}
-			{/if}
+					<h5 class="h5 opacity-60">{category}</h5>
+				</div>
 
-			{#if grayFlairs !== undefined}
-				{#each grayFlairs as flair}
-					<Flair variant="gray-chip" label={flair} />
-				{/each}
-			{/if}
-			{#if blueFlairs !== undefined}
-				{#each blueFlairs as flair}
-					<Flair variant="blue-chip" label={flair} />
-				{/each}
-			{/if}
-
-			{#if pinkFlairs !== undefined}
-				{#each pinkFlairs as flair}
-					<Flair variant="pink-chip" label={flair} />
-				{/each}
-			{/if}
-			<div />
+				<span class="h1 opacity-80">${price}</span>
+			</div>
 		</div>
-
-		<ProductImage {imgSrc} {imgAlt} />
-
-		<ProductDetails {name} {category} {price} />
-	</a>
+	</div>
 {:else if size === 'small'}
-	<a
-		href={url}
-		id="small"
-		class="isolate w-full tablet-sm:max-w-[calc(50%-12px)] laptop:max-w-[calc(25%-18px)] flex flex-col justify-center items-center gap-24 p-12 pb-24 hover:bg-white/5 rounded-t-[32px] rounded-b relative outline outline-2 outline-transparent outline-offset-4 focus-visible:outline-current"
+	<div
+		id="product-card__small"
+		class="isolate w-full laptop:max-w-[calc(50%-12px)] h-fit tablet-sm:h-240 flex flex-col tablet-sm:flex-row justify-center items-center gap-24 bg-black relative outline outline-2 outline-transparent outline-offset-4 focus-visible:outline-current"
 	>
 		<div
-			id="flairs"
-			class="absolute top-0 right-0 z-10 w-full p-36 flex justify-start items-start gap-4"
+			class="isolate w-full h-full max-h-240 flex justify-center items-center rounded border border-white/20 overflow-hidden relative"
 		>
-			{#if greenFlairs !== undefined}
-				{#each greenFlairs as flair}
-					<Flair variant="green-chip" label={flair} />
-				{/each}
-			{/if}
+			<img src={imgSrc} alt={imgAlt} class="w-full h-full object-cover" />
 
-			{#if orangeFlairs !== undefined}
-				{#each orangeFlairs as flair}
-					<Flair variant="orange-chip" label={flair} />
-				{/each}
-			{/if}
+			<div
+				id="flairs"
+				class="w-fit max-w-[15rem] justify-start items-start gap-4 absolute top-0 left-0 p-24"
+			>
+				{#if greenFlairs !== undefined}
+					{#each greenFlairs as flair}
+						<Flair variant="green-chip" label={flair} />
+					{/each}
+				{/if}
 
-			{#if grayFlairs !== undefined}
-				{#each grayFlairs as flair}
-					<Flair variant="gray-chip" label={flair} />
-				{/each}
-			{/if}
-			{#if blueFlairs !== undefined}
-				{#each blueFlairs as flair}
-					<Flair variant="blue-chip" label={flair} />
-				{/each}
-			{/if}
+				{#if orangeFlairs !== undefined}
+					{#each orangeFlairs as flair}
+						<Flair variant="orange-chip" label={flair} />
+					{/each}
+				{/if}
 
-			{#if pinkFlairs !== undefined}
-				{#each pinkFlairs as flair}
-					<Flair variant="pink-chip" label={flair} />
-				{/each}
-			{/if}
-			<div />
+				{#if grayFlairs !== undefined}
+					{#each grayFlairs as flair}
+						<Flair variant="gray-chip" label={flair} />
+					{/each}
+				{/if}
+				{#if blueFlairs !== undefined}
+					{#each blueFlairs as flair}
+						<Flair variant="blue-chip" label={flair} />
+					{/each}
+				{/if}
+
+				{#if pinkFlairs !== undefined}
+					{#each pinkFlairs as flair}
+						<Flair variant="pink-chip" label={flair} />
+					{/each}
+				{/if}
+				<div />
+			</div>
+
+			<div
+				class="w-full flex flex-row flex-wrap justify-start items-start gap-4 p-24 absolute bottom-0 left-0"
+			>
+				<Button as="button" variant="solid-black" on:click={addItemToCart}>
+					<span>add to cart</span>
+				</Button>
+
+				<Button as="link" {url} variant="solid-black">
+					<span>see details</span>
+				</Button>
+			</div>
 		</div>
 
-		<ProductImage {imgSrc} {imgAlt} />
+		<div
+			id="product-details-wrapper"
+			class="isolate w-full tablet-sm:max-w-[15rem] h-full flex flex-col justify-start items-start gap-24 p-24 bg-black rounded border border-white/20 overflow-hidden"
+		>
+			<div class="w-full max-w-[22.5rem] h-full flex flex-col justify-start items-start gap-24">
+				<div class="flex flex-col gap-12">
+					<h4 class="h4">{name}</h4>
 
-		<ProductDetails {name} {category} {price} />
-	</a>
+					<h5 class="h5 opacity-60">{category}</h5>
+				</div>
+
+				<span class="h1 opacity-80">${price}</span>
+			</div>
+		</div>
+	</div>
 {/if}
