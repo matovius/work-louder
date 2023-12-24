cd06999e817712cecbb785894bbeed2c3808a8c1
Martin Matovu <matovius@proton.me>
2023-12-24 16:37:09 +0300

    Add smiley icon

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export * from './components/Header.svelte';
 export * from './components/icons/WLLogoFull.svelte';
 export * from './components/icons/WLLogoShort.svelte';
+export * from './components/icons/WLSmiley.svelte';
 export * from './components/Marquee.svelte';
 export * from './components/Footer.svelte';
 export * from './components/Button.svelte';
