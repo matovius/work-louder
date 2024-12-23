9f75976ede3104b4fe0912852be92f5bfa40eab0
Martin Matovu <matovius@proton.me>
2024-12-23 12:17:45 +0300

    feat: add a design-system preview for dev

---
+++ src/lib/components/DesignSystem.svelte
@@ -0,0 +1,87 @@
+<div class="palettes">
+  <div class="palette neutral">
+    <div class="box" style="background: var(--clr-neutral-50, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-100, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-200, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-300, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-400, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-500, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-600, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-700, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-800, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-900, red);"></div>
+    <div class="box" style="background: var(--clr-neutral-950, red);"></div>
+  </div>
+  <div class="palette primary">
+    <div class="box" style="background: var(--clr-primary-50, red);"></div>
+    <div class="box" style="background: var(--clr-primary-100, red);"></div>
+    <div class="box" style="background: var(--clr-primary-200, red);"></div>
+    <div class="box" style="background: var(--clr-primary-300, red);"></div>
+    <div class="box" style="background: var(--clr-primary-400, red);"></div>
+    <div class="box" style="background: var(--clr-primary-500, red);"></div>
+    <div class="box" style="background: var(--clr-primary-600, red);"></div>
+    <div class="box" style="background: var(--clr-primary-700, red);"></div>
+    <div class="box" style="background: var(--clr-primary-800, red);"></div>
+    <div class="box" style="background: var(--clr-primary-900, red);"></div>
+    <div class="box" style="background: var(--clr-primary-950, red);"></div>
+  </div>
+</div>
+
+<div class="typefaces">
+  <h1>The quick brown fox jumps over the lazy dog</h1>
+  <h2>The quick brown fox jumps over the lazy dog</h2>
+  <h3>The quick brown fox jumps over the lazy dog</h3>
+  <h4>The quick brown fox jumps over the lazy dog</h4>
+  <h5>The quick brown fox jumps over the lazy dog</h5>
+  <h6>The quick brown fox jumps over the lazy dog</h6>
+  <p>The quick brown fox jumps over the lazy dog</p>
+  <small>The quick brown fox jumps over the lazy dog</small>
+</div>
+
+<div class="buttons">
+  <button class="btn btn-default">
+    <span>Default Button</span>
+  </button>
+  <button class="btn btn-inverse">
+    <span>Inverse Button</span>
+  </button>
+  <button class="btn btn-primary">
+    <span>Primary Button</span>
+  </button>
+  <button class="btn btn-ghost">
+    <span>Ghost Button</span>
+  </button>
+</div>
+
+<style>
+  .palettes {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    & > div.palette {
+      display: flex;
+      flex-direction: row;
+    }
+  }
+
+  .box {
+    width: 100px;
+    height: 100px;
+    aspect-ratio: 1;
+  }
+
+  .typefaces {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
+
+  .buttons {
+    padding-block-start: 24px;
+
+    & > button.btn {
+      --btn-outline-color: var(--clr-white);
+    }
+  }
+</style>
---
+++ src/routes/ds/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { dev } from "$app/environment";
+  import DesignSystem from "$lib/components/DesignSystem.svelte";
+</script>
+
+<svelte:head>
+  {#if dev}
+    <title>The design system of work_louder</title>
+  {:else}
+    <title>Nothing to see here...</title>
+  {/if}
+</svelte:head>
+
+{#if dev}
+  <DesignSystem />
+{:else}
+  <p class="p">There's absolutely nothing to see here. Be on your way...</p>
+{/if}
