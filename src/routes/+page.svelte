52bda57c59e71876171a80b4a7fd91083f5e3080
Martin Matovu <matovius@proton.me>
2024-12-26 11:58:52 +0300

    move newletter cta and wrk.shop cts to separate components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+  import NewsletterCTACard from '$lib/components/NewsletterCTACard.svelte';
+  import WrkShopCTACard from '$lib/components/WrkShopCTACard.svelte';
   import InputCTACard from '$lib/components/InputCTACard.svelte';
-
 	import Footer from '$lib/components/Footer.svelte';
   import Header from '$lib/components/Header.svelte';
 	import Marquee from '$lib/components/Marquee.svelte';
@@ -97,39 +98,8 @@
   </Marquee>
 
   <section id="extras" class="extras-section">
-    <div class="container wrk-shop">
-      <div class="bg"></div>
-      <div class="content">
-        <h2>visit the wrk.shop</h2>
-        <p>It's the place where your renders become reality.</p>
-        <button class="btn btn-default" aria-label="visit">
-          <span aria-hidden="true">visit</span>
-        </button>
-      </div>
-    </div>
-
-    <div class="container newsletter">
-      <div class="bg">
-      </div>
-      <div class="content">
-        <h4>join the newsletter</h4>
-        <p>Get product updates and offers straight to your inbox.<br>No spam, we promise.</p>
-      </div>
-
-      <div class="form">
-        <form>
-          <label for="email">
-            <p>Email address</p>
-            <input type="email" name="email" id="email" class="input" placeholder="e.g. person@example.com" />
-          </label>
-          <div class="cta">
-            <button class="btn btn-default" aria-label="join now">
-              <span aria-hidden="true">join now</span>
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <WrkShopCTACard />
+    <NewsletterCTACard />
   </section>
 </main>
 
@@ -214,95 +184,9 @@
     flex-direction: column;
     gap: .75rem; /* 12px */
     padding-block-end: 3rem; /* 48px */
-
-    & > .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 3rem 1.5rem; /* 48px 24px */
-      border-radius: 24px;
-      isolation: isolate;
-      position: relative;
-      overflow: hidden;
-      
-      & > .bg {
-        background: var(--clr-neutral-800);
-        position: absolute;
-        inset: 0;
-        z-index: -1;
-      }
-    }
-  }
-  section.extras-section > .container.wrk-shop {
-
-    & > .content {
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 1.5rem; /* 24px */
-    }
-    & button.btn.btn-default {
-      --btn-padding: .75rem 3rem; /* 12px 48px */
-    }
-  }
-  section.extras-section > .container.newsletter {
-    height: var(--_height);
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    gap: 1.5rem; /* 24px */
-    padding-block: 1.5rem; /* 24px */
-
-    @media screen and (min-width: 37.5rem) /* 600px */ {
-      flex-direction: row;
-    }
-
-    & *::selection {
-      color: var(--clr-white);
-      background: var(--clr-black);
-    }
-
-    & > .bg {
-      display: flex;
-      justify-content: center;
-      align-items: flex-start;
-      background: var(--clr-white);
-      background-image: radial-gradient(circle at 0% 50%, var(--clr-primary-500), transparent 60%);
-      padding-inline: 12px;
-    }
-    
-    & > .content {
-      color: var(--clr-black);
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: flex-start;
-      gap: .75rem; /* 12px */
-    }
-
-    & > .form {
-      width: 100%;
-      max-width: 22.5rem; /* 360px */
-      color: var(--clr-black);
-    }
-
-    & .cta {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      gap: .75rem; /* 12px */
-      padding-block-start: .25rem; /* 4px */
-
-      & > button.btn {
-        --btn-padding: .75rem 1.5rem; /* 12px 24px */
-      }
-    }
-
   }
 
+  /* MARQUEE TEXT */
   span.marquee-text {
     & > s {
       color: var(--clr-primary-500);
